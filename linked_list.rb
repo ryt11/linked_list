eead9a38eda92d760491265128af71971098e8ad
ryt11 <rileyt_2000@hotmail.com>
2017-03-14 23:44:24 -0600

    add working push method

---
+++ linked_list.rb
@@ -0,0 +1,25 @@
+require_relative 'node'
+require 'pry'
+
+class LinkedList
+  attr_accessor :nodes
+  def initialize
+    @nodes = []
+  end
+  def push(val)
+    nodes.empty? ? nodes << Node.new(val, nil) : nodes[nodes.length] = Node.new(val, nil)
+    nodes[nodes.length-2].next_node = nodes[nodes.length-1]
+  end
+end
+
+ll = LinkedList.new
+
+ll.push("Hello")
+ll.push("Herp")
+ll.push("Hedf")
+ll.push("Hesa")
+ll.push("He34o")
+ll.push("He12")
+whats_this = ll.nodes.count
+binding.pry
+''
---
+++ node.rb
@@ -0,0 +1,7 @@
+class Node
+  attr_accessor :val, :next_node
+  def initialize(val, next_node)
+    @val = val
+    @next_node = next_node
+  end
+end
